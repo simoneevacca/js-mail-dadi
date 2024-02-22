db3a140ec40650a3f7c3ae765229cbea087929b5
simoneevacca <v.simone.vacca@gmail.com>
2024-02-22 14:29:05 +0100

    create variable and condition. Exercise done.

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,3 +6,16 @@
 
 // check if the insert mail is correct
 
+const userMail = prompt ('inserisci la tua mail');
+console.log(userMail);
+
+let array = ['simone@mail.com', 'francesco@mail.com', 'marco@mail.com'];
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+
+    if (element == userMail) {
+        alert ("Congratulazioni!! Puoi accedere al sito!")
+    }
+    
+}
